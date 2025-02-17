630215c5a6e2883ad7088d3d15048ce403dc47f9
jhs512 <jangka512@gmail.com>
2025-02-17 12:21:02 +0900

    work

--- src/main/kotlin/com/ll/global/bean/SingletonScope.kt
+++ src/main/kotlin/com/ll/global/bean/SingletonScope.kt
@@ -3,14 +3,13 @@ package com.ll.global.bean
 import com.ll.domain.system.system.controller.SystemController
 import com.ll.domain.wiseSaying.wiseSaying.controller.WiseSayingController
 import com.ll.domain.wiseSaying.wiseSaying.repository.WiseSayingFileRepository
-import com.ll.domain.wiseSaying.wiseSaying.repository.WiseSayingMemoryRepository
 import com.ll.domain.wiseSaying.wiseSaying.repository.WiseSayingRepository
 import com.ll.domain.wiseSaying.wiseSaying.service.WiseSayingService
 
 object SingletonScope {
     val wiseSayingController by lazy { WiseSayingController() }
     val wiseSayingService by lazy { WiseSayingService() }
-    val wiseSayingRepository: WiseSayingRepository by lazy { WiseSayingMemoryRepository() }
+    val wiseSayingRepository: WiseSayingRepository by lazy { WiseSayingFileRepository() }
     val wiseSayingFileRepository by lazy { WiseSayingFileRepository() }
     val systemController by lazy { SystemController() }
 }

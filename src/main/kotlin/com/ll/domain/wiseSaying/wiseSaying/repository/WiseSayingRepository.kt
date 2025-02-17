9a966da34afd502cde29552ef88a38eeb0adc39a
jhs512 <jangka512@gmail.com>
2025-02-17 11:14:05 +0900

    work

--- src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/repository/WiseSayingRepository.kt
+++ src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/repository/WiseSayingRepository.kt
@@ -2,37 +2,11 @@ package com.ll.domain.wiseSaying.wiseSaying.repository
 
 import com.ll.domain.wiseSaying.wiseSaying.entity.WiseSaying
 
-class WiseSayingRepository {
-    private var lastId = 0
-    private val wiseSayings = mutableListOf<WiseSaying>()
-
-    fun save(wiseSaying: WiseSaying): WiseSaying {
-        if (wiseSaying.isNew()) {
-            wiseSaying.id = ++lastId
-            wiseSayings.add(wiseSaying)
-        }
-
-        return wiseSaying
-    }
-
-    fun isEmpty(): Boolean {
-        return wiseSayings.isEmpty()
-    }
-
-    fun findAll(): List<WiseSaying> {
-        return wiseSayings
-    }
-
-    fun findById(id: Int): WiseSaying? {
-        return wiseSayings.firstOrNull { it.id == id }
-    }
-
-    fun delete(wiseSaying: WiseSaying) {
-        wiseSayings.remove(wiseSaying)
-    }
-
-    fun clear() {
-        lastId = 0
-        wiseSayings.clear()
-    }
+interface WiseSayingRepository {
+    fun save(wiseSaying: WiseSaying): WiseSaying
+    fun isEmpty(): Boolean
+    fun findAll(): List<WiseSaying>
+    fun findById(id: Int): WiseSaying?
+    fun delete(wiseSaying: WiseSaying)
+    fun clear()
 }
--- src/main/kotlin/com/ll/global/bean/SingletonScope.kt
+++ src/main/kotlin/com/ll/global/bean/SingletonScope.kt
@@ -2,12 +2,13 @@ package com.ll.global.bean
 
 import com.ll.domain.system.system.controller.SystemController
 import com.ll.domain.wiseSaying.wiseSaying.controller.WiseSayingController
+import com.ll.domain.wiseSaying.wiseSaying.repository.WiseSayingMemoryRepository
 import com.ll.domain.wiseSaying.wiseSaying.repository.WiseSayingRepository
 import com.ll.domain.wiseSaying.wiseSaying.service.WiseSayingService
 
 object SingletonScope {
     val wiseSayingController by lazy { WiseSayingController() }
     val wiseSayingService by lazy { WiseSayingService() }
-    val wiseSayingRepository by lazy { WiseSayingRepository() }
+    val wiseSayingRepository: WiseSayingRepository by lazy { WiseSayingMemoryRepository() }
     val systemController by lazy { SystemController() }
 }

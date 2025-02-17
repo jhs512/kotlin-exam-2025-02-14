113891fb4498bf653d21efc40f4ab889760dbf6a
jhs512 <jangka512@gmail.com>
2025-02-17 11:48:31 +0900

    work

--- src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/repository/WiseSayingFileRepository.kt
+++ src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/repository/WiseSayingFileRepository.kt
@@ -33,7 +33,11 @@ class WiseSayingFileRepository : WiseSayingRepository {
     }
 
     override fun findById(id: Int): WiseSaying? {
-        return wiseSayings.firstOrNull { it.id == id }
+        return tableDirPath
+            .toFile()
+            .listFiles()
+            ?.find { it.name == "${id}.json" }
+            ?.let { WiseSaying.fromJsonStr(it.readText()) }
     }
 
     override fun delete(wiseSaying: WiseSaying) {
@@ -43,6 +47,8 @@ class WiseSayingFileRepository : WiseSayingRepository {
     override fun clear() {
         lastId = 0
         wiseSayings.clear()
+
+        tableDirPath.toFile().deleteRecursively()
     }
 
     private fun saveOnDisk(wiseSaying: WiseSaying) {
